c65e8cb506c60dbcda3acfdbc5c5dd2ee01617aa
Vinith Arokiasamy <48160545+devil-V-white@users.noreply.github.com>
2022-03-07 08:30:13 -0800

    Add files via upload

---
+++ crop.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image, ImageOps
+from keras.models import model_from_json
+import cv2 
+from PIL import Image
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+
+st.write("""
+          # Crop Disease Identification
+          """
+          )
+upload_file = st.sidebar.file_uploader("Upload Crop Leaf Images", type="jpg")
+Generate_pred=st.sidebar.button("Predict")
+json_file = open('Grape_Model.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+#load woeights into new model
+loaded_model.load_weights("GrapeModel_weights.h5")
+def import_n_pred(image_data, model):
+    size = (256,256)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    reshape=img[np.newaxis,...]
+    prediction = loaded_model.predict(reshape)
+    return prediction
+    
+if upload_file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(upload_file)
+    with st.expander('Crop Image', expanded = True):
+        st.image(image, use_column_width=True)
+    prediction=import_n_pred(image, model)
+    class_labels=['Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy']
+    st.title("{}".format(class_labels[np.argmax(prediction)]))
+    if np.argmax(prediction)==0:
+        st.header('\nTreatment:\nspraying Bordeaux mixture (4:4:100) once or twice on young bunches prevents the infection')
+    elif np.argmax(prediction)==1:
+        st.header('\nTreatment:\nProphylactic sprays with Captan (0.2%) and Benomyl or Bavistin(Carbendazim) (0.1%) minimize the development of the fungus during transit and storage.')
+    elif np.argmax(prediction)==2:
+        st.header('\nTreatment:\nSpraying of the grapevines at 3-4 leaf stage with fungicides like Bordeaux mixture @ 0.8% or Copper Oxychloride @ 0.25% or Carbendazim @ 0.1% are effective against    this disease.')
+    elif np.argmax(prediction)==3:
+        st.header('\nPlants are healthy')
